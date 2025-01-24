eba71fa1081d893a3fc5a331ca9c678f9391a3fb
Vanessa Andrade <v.andradd@gmail.com>
2025-01-24 19:58:29 -0300

    Banco de Dados conexao
    Banco de dados MongoDB
    Mongoose
    Conexão ao banco de dados MongoDB

--- src/app.js
+++ src/app.js
@@ -1,53 +1,49 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+})
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id:2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro =>{
-        return livro.id === Number(id);
-    })
-}
-
 app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
+  res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res)=> {
-    res.status(200).json(livros);
-})
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
+});
 
 app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
 })
 
 app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("livro cadastrado com sucesso");
+  livros.push(req.body);
+  res.status(201).send("livro cadastrado com sucesso");
 });
 
-app.put("/livros/:id", (req, res) =>{
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status (200).json(livros);
-})
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
 
 app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("livro removido com sucesso");
-})
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
 
 export default app;
